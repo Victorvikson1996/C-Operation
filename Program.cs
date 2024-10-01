87d308a3691cefaccbfe53bbdd33c378f35ff13d
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2024-10-01 13:29:53 +0200

    Initial Commmit OPerartion

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = 23;
+            age++;
+            age += 10;
+            age -= 10;
+            string name = "ABA";
+            name += " is programm";
+
+            char ch = 'a';
+            ch += 'b';
+            Console.WriteLine(ch);
+
+            Console.WriteLine(name);
+            int i = 0;
+            Console.WriteLine(i++);
+            Console.WriteLine(age);
+            age /= 10;
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+        }
+    }
+}
+
